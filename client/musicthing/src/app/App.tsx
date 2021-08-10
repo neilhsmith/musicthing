c3adf2e6e59a4ccb174639ae4a3e53bb933a275a
Neil Smith <me+github@neilsmith.fyi>
2021-08-10 17:57:53 -0500

    added LoginPage placeholder

--- client/musicthing/src/app/App.tsx
+++ client/musicthing/src/app/App.tsx
@@ -2,11 +2,12 @@ import "styles/main.scss";
 import "./app.styles.scss";
 
 import { PlayerPage } from "features/player/components";
+import { LoginPage } from "features/auth/components";
 
 export default function App() {
   return (
     <div className="app theme-dark" data-testid="app">
-      <PlayerPage />
+      <LoginPage />
     </div>
   );
 }
---
+++ client/musicthing/src/features/auth/components/LoginPage/LoginPage.tsx
@@ -0,0 +1,5 @@
+import Page from "common/layout/Page";
+
+export default function LoginPage() {
+  return <Page>login page</Page>;
+}
