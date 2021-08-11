6e8a12b932d76f04b2bc910ef47e68fccffd5e48
Neil Smith <me+github@neilsmith.fyi>
2021-08-10 20:38:51 -0500

    add poppins font

--- client/musicthing/src/app/App.tsx
+++ client/musicthing/src/app/App.tsx
@@ -1,6 +1,8 @@
 import "styles/main.scss";
 import "./app.styles.scss";
 
+import "typeface-poppins";
+
 import { PlayerPage } from "features/player/components";
 import { LoginPage } from "features/auth/components";
 
